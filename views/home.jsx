05e0fbc57cf76e4efffae8d8a595db0e530c3d7a
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-04-27 10:49:11 -0700

    Fixing errors

--- views/home.jsx
+++ views/home.jsx
@@ -7,8 +7,8 @@ function home () {
            <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src="/images/steak.jpg" alt="steak-image" />
-                    <img src ="/images/cake.jpg" alt="cake image" />
+                    <img src="/images/steak.jpg" alt="steak-image" width="600px"/>
+                    {/* <img src ="/images/cake.jpg" alt="cake image" /> */}
                 <div>
                     Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
             </div>
